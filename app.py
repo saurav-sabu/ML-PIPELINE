635640ede087f17428710bcd7115b5d41a80307e
Saurav Sabu <saurav.sabu9@gmail.com>
2023-11-12 00:46:19 +0530

    Updated app.py. Added comments

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
+# Import Flask
 from flask import Flask,render_template,request,jsonify
 
+# Import necessary classes and functions from your custom modules
 from src.pipeline.prediction_pipeline import PredictionPipeline,CustomClass
 
+# Create Flask instance
 app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def prediction_data():
+    # Handle GET request to render the home page
     if request.method == "GET":
         return render_template("home.html")
     else:
+        # Handle POST request to process form data and make predictions
+
+        # Create an instance of CustomClass with form data
         data = CustomClass(
             age = int(request.form.get("age")),
             workclass= int(request.form.get("workclass")),
@@ -24,18 +31,24 @@ def prediction_data():
             native_country=int(request.form.get("native_country"))
         )
 
+    # Extract a DataFrame from the CustomClass instance
     final_data = data.get_dataframe()
+
+    # Create an instance of PredictionPipeline
     pipeline_prediction = PredictionPipeline()
 
+    # Make predictions using the pipeline
     predict = pipeline_prediction.predict(final_data)
 
+    # Determine the result based on the prediction
     result = predict
-
+    
+    # Render the appropriate result template based on the prediction result
     if result == 0:
         return render_template("results.html",final_result=f"You Income is less than equal $50k:{result}")
     else:
         return render_template("results.html",final_result=f"You Income is more than $50k:{result}")
     
-
+# Run the Flask app if the script is executed
 if __name__ == "__main__":
     app.run(debug=True)
